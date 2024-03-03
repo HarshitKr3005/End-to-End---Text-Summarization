6c4b8485d06470d004092dc7351ce7d2f6954b00
HarshitKr3005 <harshitkr3002@gmail.com>
2024-03-03 18:06:04 +0530

    completed

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-End---Text-Summarization"
+REPO_NAME = "Text-Summarizer-Project"
 AUTHOR_USER_NAME = "HarshitKr3005"
-SRC_REPO = "TextSummarizer"
-AUTHOR_EMAIL = "harshitkr3002@gmail.com"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "harshitkr3005@gmail.com"
+
+
 
 setuptools.setup(
     name=SRC_REPO,
